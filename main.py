2726f961a64514499d0464f5a4435bc7b9352756
obaid-786 <obaidsid0@gmail.com>
2026-09-10 20:46:31 +0530

    Allow all origins for initial deploy

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from routes import problems,attempts
 app = FastAPI(title="lld practice")
 
 # allow cors for local and frontend
-app.add_middleware(CORSMiddleware, allow_origins = ["http://localhost:3000","http://127.0.0.1:3000",],allow_credentials=True,allow_methods =["*"], allow_headers=["*"],)
+app.add_middleware(CORSMiddleware, allow_origins = ["http://localhost:3000","http://127.0.0.1:3000",],allow_origins=["*"],allow_credentials=True,allow_methods =["*"], allow_headers=["*"],)
 
 app.include_router(problems.router)
 app.include_router(attempts.router)
